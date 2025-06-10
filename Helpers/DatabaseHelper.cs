9ce865aed2568ccb23b1cbaeb4883ea8ac67e2eb
LPrabodha <lprabodha1998@gmail.com>
2025-06-10 18:44:05 +0530

    Implement Database Helper

--- Helpers/DatabaseHelper.cs
+++ Helpers/DatabaseHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace eShift_Logistics_System.Helpers
 {
-    class DatabaseHelper
+    public static class DatabaseHelper
     {
+        private static readonly string connectionString = "Server=localhost;Database=eshift_db;Uid=root;Pwd=";
+
+        public static MySqlConnection GetConnection()
+        {
+            try
+            {
+                var connection = new MySqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Failed to create a database connection.", ex);
+            }
+        }
+
+        // Executes an insert, update, or delete command
+        public static int ExecuteNonQuery(string query, params MySqlParameter[] parameters)
+        {
+
+            using (var conn = GetConnection())
+            using (var cmd = new MySqlCommand(query, conn))
+            {
+
+                if(parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                return cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        // Executes a scalar command (returns a single value)
+        public static object ExecuteScalar(string query, params MySqlParameter[] parameters)
+        {
+            using (var conn = GetConnection())
+            using (var cmd = new MySqlCommand(query, conn))
+            {
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                return cmd.ExecuteScalar();
+            }
+        }
     }
 }
