365295e8df59af6ff5fe64812a489621189d3253
LPrabodha <lprabodha1998@gmail.com>
2025-07-15 02:02:29 +0530

    Add XML documentation to IEmailService methods
    Updated the `IEmailService` interface in `IEmailService.cs` to include XML documentation comments for each method. This change improves code readability and provides better context for developers by summarizing the purpose of each method and detailing the parameters they accept.

--- Business/Interface/IEmailService.cs
+++ Business/Interface/IEmailService.cs
@@ -9,9 +9,29 @@ namespace eShift_Logistics_System.Business.Interface
 {
     public interface IEmailService
     {
+        /// <summary>
+        /// Sends a confirmation email to the customer when a job is created.
+        /// </summary>
+        /// <param name="job"></param>
         void SendJobConfirmationEmail(Job job);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="job"></param>
         void SendJobApprovedEmail(Job job);
+
+        /// <summary>
+        /// Sends an email to the customer when a job is declined.
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="newStatus"></param>
         void SendJobStatusUpdateEmail(Job job, JobStatus newStatus);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="job"></param>
         void SendJobCompletionEmailWithInvoice(Job job);
     }
 }
