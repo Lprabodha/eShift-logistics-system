c65c78921aec080272704684ad881c407f19e90c
LPrabodha <lprabodha1998@gmail.com>
2025-07-15 01:59:52 +0530

    Refactor email handling and implement IEmailService
    The EmailHelper class has been refactored to a static class, consolidating email sending functionality and introducing a new SendEmail method with optional attachments. SMTP configuration has been added using MimeKit and MailKit, along with error handling for attachments.
    
    A new IEmailService interface has been created to define methods for sending job-related emails, establishing a contract for email services. The EmailService class implements this interface, providing methods for sending various job-related emails and generating PDF invoices to be sent as attachments.

---
+++ Business/Interface/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using eShift_Logistics_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShift_Logistics_System.Business.Interface
+{
+    public interface IEmailService
+    {
+        void SendJobConfirmationEmail(Job job);
+        void SendJobApprovedEmail(Job job);
+        void SendJobStatusUpdateEmail(Job job, JobStatus newStatus);
+        void SendJobCompletionEmailWithInvoice(Job job);
+    }
+}
